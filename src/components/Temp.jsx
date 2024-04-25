f372a7c2bba3abfc62079b98c3ef02ebad6e02ba
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-04-25 19:31:41 +0530

    static completed

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Temp() {
   return (
-    <div className='text-4xl tracking-wide text-gray-700'>38°C</div>
+    <div className='text-4xl font-sans tracking-wide text-gray-700'>38°C</div>
   )
 }
--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import {data} from '../constants/data'
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { TiWeatherSunny } from "react-icons/ti";
+import { TiWeatherShower  } from "react-icons/ti";
+
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 12,
+    items: 9,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 9,
+    items: 6,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -24,14 +29,15 @@ const responsive = {
 export default function TimeLine() {
   return (
     <div>
-      <Carousel responsive={responsive} className="space-y-4">
-        <div className="h-[200px] p-10 mt-10 mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        <div className="h-[200px] p-10 mt-4  mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        <div className="h-[200px] p-10 mt-10 mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        <div className="h-[200px] p-10 mt-10 mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        <div className="h-[200px] p-10 mt-10  mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        <div className="h-[200px] p-10 mt-10  mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full">Item 1</div>
-        
+      <Carousel responsive={responsive}>
+        {console.log(data.timelines.hourly)}
+        {data.timelines.hourly.slice(0,24).map((item,index)=>(
+          <div key={index} className="h-[200px] pt-10 mt-10 mb-10 w-full bg-sky-200 border-sky-300 border-2 rounded-full text-center">
+            <span className="font-serif text-xl">{item.time.split('T')[1].split(":")[0] == 12 ? <span>12 PM</span> : item.time.split('T')[1].split(":")[0] > 12 ? <span>{item.time.split('T')[1].split(":")[0]-12} PM</span>:<span>{item.time.split('T')[1].split(":")[0]} AM</span>}</span>
+            {Math.ceil(item.values.temperature)>22?<TiWeatherSunny className="text-5xl mt-3 mx-6 mb-2"/> : Math.ceil(item.values.temperature)>19?<TiWeatherPartlySunny className="text-5xl mt-3 mx-6 mb-2"/>:<TiWeatherShower  className="text-5xl mt-3 mx-6 mb-2" />}
+            <span className="font-serif text-xl">{Math.ceil(item.values.temperature)}°C</span>
+          </div>
+        ))}
       </Carousel>
     </div>
   );
